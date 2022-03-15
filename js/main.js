cfb26fefedb0d7576d1d0554b809e070c3279a56
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2022-03-15 17:05:23 +0900

    youtube API

--- js/main.js
+++ js/main.js
@@ -82,3 +82,27 @@ promotiontoggleBtn.addEventListener('click', function () {
     promotionEl.classList.add('hide');
   }
 })
+
+gsap.to('.floating1', 1.5, {
+  delay: 1,               // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정
+  y: 15,                  // transform: translateY(수치); 와 같음. 수직으로 얼마나 움직일지
+  repeat: -1,             // 몇 번 반복하는지를 설정, -1 은 무한 반복
+  yoyo: true,             // 한번 재생된 애니메이션을 다시 뒤로 재생
+  ease: Power1.easeInOut  // Easing 함수 적용
+})
+
+gsap.to('.floating2', 2, {
+  delay: .5,
+  y: 15,
+  repeat: -1,
+  yoyo:true,
+  ease: Power1.easeInOut
+})
+
+gsap.to('.floating3', 2.5, {
+  delay: 1.5,
+  y: 20,
+  repeat: -1,
+  yoyo:true,
+  ease: Power1.easeInOut
+})
