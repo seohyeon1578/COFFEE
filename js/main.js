b8f05ff19e1a717d20a5d49b6611a0c0c58e787d
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2022-03-16 13:25:45 +0900

    bugfix

--- js/main.js
+++ js/main.js
@@ -89,14 +89,14 @@ new Swiper('.promotion .swiper', {
 });
 
 new Swiper('.awards .swiper', {
-  autoplay: true,
-  loop: true,
-  spaceBetween: 30,
-  slidesPerView:5,
   navigation: {
     prevEl: '.awards .swiper-button-prev',
     nextEl: '.awards .swiper-button-next'
-  }
+  },
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView:5
 });
 
 const promotionEl = document.querySelector('section.promotion');
